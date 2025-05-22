f69b0ac3af39d69825e6b64ef37d018eb0b37d19
zawpi <155113964+zawpi@users.noreply.github.com>
2025-05-22 03:51:38 +0200

    Update Hooks.h

--- ImGui DirectX 11 Kiero Hook/Cheats/Hooks.h
+++ ImGui DirectX 11 Kiero Hook/Cheats/Hooks.h
@@ -3,23 +3,16 @@
 #include "../Utils/Variable.h"
 #include "../Utils/Callfunction.h"
 
-
-
 void(__fastcall* Throw_o)(void* self, Vector3 forward, Vector3 position, Quaternion rotation, std::byte utilIndex);
 void __stdcall Throw_hook(void* self, Vector3 forward, Vector3 position, Quaternion rotation, std::byte utilIndex) {
 	std::cout << (int)utilIndex << std::endl;
-
-
+	
 	for (int i = 0; i < Menu::NadeMultiply; i++)
 	{
 		Throw_o(self, Vector3(forward.x * Menu::NadeVeloMultiply, forward.y * Menu::NadeVeloMultiply, forward.z * Menu::NadeVeloMultiply), position, rotation, utilIndex);
-
 		if (Menu::Nuke)
 		{
-
-		
 			for (void* playerVoid : Variable::Players) {
-
 				std::cout << "throw";
 				uintptr_t player = (uintptr_t)playerVoid;
 				void* movementManag = *reinterpret_cast<void**>((uintptr_t)(player)+Offsets::playermovement_offset);
@@ -28,20 +21,16 @@ void __stdcall Throw_hook(void* self, Vector3 forward, Vector3 position, Quatern
 			}
 		}
 	}
-
-
-
 }
 
 
 void(__fastcall* PlayerStart_o)(void* self);
 void __stdcall PlayerStart_hook(void* self) {
-
 	PlayerStart_o(self);
 	std::cout << "player created \n";
-
 	void* playeridentity = *reinterpret_cast<void**>((uintptr_t)(self)+Offsets::playeridentity_offset);
 	std::cout << "player identity \n";
+	
 	if (*reinterpret_cast<bool*>((uintptr_t)playeridentity + 0x22) == true)
 	{
 		std::cout << "player is local \n";
@@ -55,13 +44,11 @@ void __stdcall PlayerStart_hook(void* self) {
 		std::cout << "Added Player To List";
 		Variable::Players.insert(self);
 	}
-
 }
 
 
 void(__fastcall* PlayerDestroy_o)(void* self);
 void __stdcall PlayerDestroy_hook(void* self) {
-
 	void* playeridentity = *reinterpret_cast<void**>((uintptr_t)self + Offsets::playeridentity_offset);
 	if (*reinterpret_cast<bool*>((uintptr_t)playeridentity + 0x22) == true)
 	{
@@ -75,17 +62,12 @@ void __stdcall PlayerDestroy_hook(void* self) {
 		Variable::Players.erase(self);
 	}
 	PlayerDestroy_o(self);
-
 }
-
-
 
 void(__fastcall* Function_o)(void* self);
 void __stdcall Function_hook(void* self) {
-
 	std::cout << "gun created";
 	Function_o(self);
-
 }
 
 
